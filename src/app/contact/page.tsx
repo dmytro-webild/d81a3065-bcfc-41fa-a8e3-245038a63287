1900cc69b36f99f6cf63ece91cd05d8ef495c67a
bender <kudin.dima@gmail.com>
2026-03-03 10:04:24 +0000

    Update src/app/contact/page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,7 +16,7 @@ export default function ContactPage() {
       borderRadius="rounded"
       contentWidth="mediumSmall"
       sizing="mediumSizeLargeTitles"
-      background="grid"
+      background="circleGradient"
       cardStyle="layered-gradient"
       primaryButtonStyle="primary-glow"
       secondaryButtonStyle="solid"
@@ -43,7 +43,7 @@ export default function ContactPage() {
           description="Мы доступны 24/7 для ответа на ваши вопросы. Позвоните, напишите в Telegram или отправьте запрос на электронную почту. Мы ответим в течение 15 минут!"
           tag="Контакты"
           tagIcon={Package}
-          background={{ variant: "grid" }}
+          background={{ variant: "plain" }}
           buttons={[
             { text: "📞 +998 93 494 10 06", href: "tel:+998934941006" },
             { text: "💬 Telegram", href: "https://t.me/salafan_chi" },
@@ -51,34 +51,22 @@ export default function ContactPage() {
           mediaItems={[
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/warehouse-logistics-service-composition_23-2149128315.jpg?_wi=4",
-              imageAlt: "Наш офис и склад",
-            },
+                "http://img.b2bpic.net/free-photo/warehouse-logistics-service-composition_23-2149128315.jpg",              imageAlt: "Наш офис и склад"},
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/export-supervisor-drafting-billing-product-logistics_482257-88029.jpg?_wi=5",
-              imageAlt: "Менеджеры по продажам",
-            },
+                "http://img.b2bpic.net/free-photo/export-supervisor-drafting-billing-product-logistics_482257-88029.jpg",              imageAlt: "Менеджеры по продажам"},
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/female-trucker-standing-front-parked-trucks-pointing-her-finger-transportation-vehicles_342744-281.jpg?_wi=3",
-              imageAlt: "Служба доставки",
-            },
+                "http://img.b2bpic.net/free-photo/female-trucker-standing-front-parked-trucks-pointing-her-finger-transportation-vehicles_342744-281.jpg",              imageAlt: "Служба доставки"},
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/rolls-garbage-bags-white-background-space-text_185193-162961.jpg?_wi=4",
-              imageAlt: "Готовая продукция",
-            },
+                "http://img.b2bpic.net/free-photo/rolls-garbage-bags-white-background-space-text_185193-162961.jpg",              imageAlt: "Готовая продукция"},
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894565.jpg?_wi=6",
-              imageAlt: "Качество материалов",
-            },
+                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894565.jpg",              imageAlt: "Качество материалов"},
             {
               imageSrc:
-                "http://img.b2bpic.net/free-photo/african-american-supervisor-evaluates-renewable-energy-metrics-factory_482257-125788.jpg?_wi=3",
-              imageAlt: "Контроль качества",
-            },
+                "http://img.b2bpic.net/free-photo/african-american-supervisor-evaluates-renewable-energy-metrics-factory_482257-125788.jpg",              imageAlt: "Контроль качества"},
           ]}
         />
       </div>
