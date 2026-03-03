e93af238ff7339cc7c6930bf4e0c5952a4202ae5
bender <kudin.dima@gmail.com>
2026-03-03 10:04:26 +0000

    Update src/app/products/page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -17,7 +17,7 @@ export default function ProductsPage() {
       borderRadius="rounded"
       contentWidth="mediumSmall"
       sizing="mediumSizeLargeTitles"
-      background="grid"
+      background="circleGradient"
       cardStyle="layered-gradient"
       primaryButtonStyle="primary-glow"
       secondaryButtonStyle="solid"
@@ -50,53 +50,23 @@ export default function ProductsPage() {
           useInvertedBackground={false}
           products={[
             {
-              id: "1",
-              name: "Полиэтиленовая плёнка 100-150 мкм",
-              price: "От 2,500 сум/кг",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894565.jpg?_wi=4",
-              imageAlt: "Полиэтиленовая плёнка премиум класса",
-            },
+              id: "1",              name: "Полиэтиленовая плёнка 100-150 мкм",              price: "От 2,500 сум/кг",              imageSrc:
+                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894565.jpg",              imageAlt: "Полиэтиленовая плёнка премиум класса"},
             {
-              id: "2",
-              name: "ПВХ трубы и фитинги",
-              price: "От 3,200 сум/кг",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/minimalist-construction-pvc-pipes-arrangement_23-2149106839.jpg?_wi=3",
-              imageAlt: "Трубы и фитинги из ПВХ",
-            },
+              id: "2",              name: "ПВХ трубы и фитинги",              price: "От 3,200 сум/кг",              imageSrc:
+                "http://img.b2bpic.net/free-photo/minimalist-construction-pvc-pipes-arrangement_23-2149106839.jpg",              imageAlt: "Трубы и фитинги из ПВХ"},
             {
-              id: "3",
-              name: "Стрейч-плёнка производственная",
-              price: "От 3,500 сум/кг",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/black-man-moving-furniture_53876-24835.jpg?_wi=3",
-              imageAlt: "Стрейч-плёнка промышленного назначения",
-            },
+              id: "3",              name: "Стрейч-плёнка производственная",              price: "От 3,500 сум/кг",              imageSrc:
+                "http://img.b2bpic.net/free-photo/black-man-moving-furniture_53876-24835.jpg",              imageAlt: "Стрейч-плёнка промышленного назначения"},
             {
-              id: "4",
-              name: "Упаковочные пакеты и рулоны",
-              price: "От 1,800 сум/кг",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/warehouse-logistics-service-composition_23-2149128315.jpg?_wi=3",
-              imageAlt: "Упаковочные материалы оптом",
-            },
+              id: "4",              name: "Упаковочные пакеты и рулоны",              price: "От 1,800 сум/кг",              imageSrc:
+                "http://img.b2bpic.net/free-photo/warehouse-logistics-service-composition_23-2149128315.jpg",              imageAlt: "Упаковочные материалы оптом"},
             {
-              id: "5",
-              name: "Салфаны и листовые материалы",
-              price: "От 2,200 сум/кг",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894571.jpg?_wi=3",
-              imageAlt: "Салфаны и пластиковые листы",
-            },
+              id: "5",              name: "Салфаны и листовые материалы",              price: "От 2,200 сум/кг",              imageSrc:
+                "http://img.b2bpic.net/free-photo/close-up-perforated-fabric_23-2149894571.jpg",              imageAlt: "Салфаны и пластиковые листы"},
             {
-              id: "6",
-              name: "Специальные заказы и нестандартные размеры",
-              price: "Цена по запросу",
-              imageSrc:
-                "http://img.b2bpic.net/free-photo/export-supervisor-drafting-billing-product-logistics_482257-88029.jpg?_wi=3",
-              imageAlt: "Производство на заказ",
-            },
+              id: "6",              name: "Специальные заказы и нестандартные размеры",              price: "Цена по запросу",              imageSrc:
+                "http://img.b2bpic.net/free-photo/export-supervisor-drafting-billing-product-logistics_482257-88029.jpg",              imageAlt: "Производство на заказ"},
           ]}
         />
       </div>
@@ -113,32 +83,16 @@ export default function ProductsPage() {
           useInvertedBackground={false}
           metrics={[
             {
-              id: "1",
-              value: "15000",
-              title: "тонн",
-              description: "Отправлено материалов за последний год",
-              icon: ShoppingCart,
+              id: "1",              value: "15000",              title: "тонн",              description: "Отправлено материалов за последний год",              icon: ShoppingCart,
             },
             {
-              id: "2",
-              value: "98",
-              title: "%",
-              description: "Клиентов возвращаются к нам повторно",
-              icon: Users,
+              id: "2",              value: "98",              title: "%",              description: "Клиентов возвращаются к нам повторно",              icon: Users,
             },
             {
-              id: "3",
-              value: "50",
-              title: "видов",
-              description: "Различных товаров в каталоге",
-              icon: CheckCircle,
+              id: "3",              value: "50",              title: "видов",              description: "Различных товаров в каталоге",              icon: CheckCircle,
             },
             {
-              id: "4",
-              value: "100",
-              title: "%",
-              description: "Сертифицировано и протестировано",
-              icon: CheckCircle,
+              id: "4",              value: "100",              title: "%",              description: "Сертифицировано и протестировано",              icon: CheckCircle,
             },
           ]}
         />
